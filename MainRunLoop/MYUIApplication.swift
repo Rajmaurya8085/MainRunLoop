7adc791cc2026eaf439db35705c5c14b35bfac90
Raj Maurya <rajmaurya8085@gmail.com>
2019-09-19 08:51:06 +0530

    create new Projectfor Main Run Loop

---
+++ MainRunLoop/MYUIApplication.swift
@@ -0,0 +1,17 @@
+//
+//  MYUIApplication.swift
+//  MainRunLoop
+//
+//  Created by Raj Maurya on 19/09/19.
+//  Copyright © 2019 Raj Maurya. All rights reserved.
+//
+
+import UIKit
+
+class  MYUIApplication: UIApplication {
+    override func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey : Any] = [:], completionHandler completion: ((Bool) -> Void)? = nil) {
+        
+    }
+    
+    
+}
